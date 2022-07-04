e867400344e198b18f6ba261ee20c537269e4475
TaVitka <tavitka17@gmail.com>
2022-07-04 10:22:56 +0300

    created a people counter in js + editted html- and css-docuents

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
-$(function(){
+let summaryPeopleCount = 0;
 
-});
+
+function peopleCounter() {
+  summaryPeopleCount = summaryPeopleCount + 1;
+  document.getElementById('counter').innerText = summaryPeopleCount;
+}

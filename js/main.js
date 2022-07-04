aa07e8600aa8953eeba5139c675c5e13fcb8affb
TaVitka <tavitka17@gmail.com>
2022-07-04 12:08:54 +0300

    finished making a people-realway-station counter

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
 let summaryPeopleCount = 0;
+let saveEl = document.getElementById('save-el');
 
 
 function peopleCounter() {
-  summaryPeopleCount = summaryPeopleCount + 1;
+  summaryPeopleCount += 1;
   document.getElementById('counter').innerText = summaryPeopleCount;
 }
+
+
+function save() {
+  let countStr = summaryPeopleCount + ' - ';
+  saveEl.textContent += countStr;
+
+  summaryPeopleCount = 0;
+  document.getElementById('counter').innerText = 0;
+}
+
+
